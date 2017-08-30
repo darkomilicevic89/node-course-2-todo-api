c26f665d2aa69677a84396d9973105f9cfe4dcf3
Darko Milicevic <darkomilicevic89@outlook.com>
2017-08-30 19:42:11 +0200

    get /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
+var {ObjectID} = require('mongodb');
+
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
@@ -26,6 +28,24 @@ app.get('/todos', (req, res) => {
     res.send({todos}); // bolje ovako nego samo todos
   }, (e) => {
     res.status(400).send(e);
+  });
+});
+
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      res.status(404).send();
+    }
+
+    res.send({todo}); // moze i samo todo, ali ovako kao bolje (isto sto i todo: todo, pa dobijemo u response todo: {..})
+  }).catch((e) => {
+    res.status(400).send();
   });
 });
 
